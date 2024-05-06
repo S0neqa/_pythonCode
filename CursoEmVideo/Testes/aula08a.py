d41ac06dc44e82864038d586f519986ecb3c4094
S0neqa <166238806+S0neqa@users.noreply.github.com>
2024-05-06 09:41:22 -0300

    Aula 08

---
+++ CursoEmVideo/Testes/aula08a.py
@@ -0,0 +1,6 @@
+import math
+
+num = int(input('digite um numero: '))
+raiz = math.sqrt(num)
+
+print(f'A raiz de {num} é igual a {raiz:.4f}!')
---
+++ CursoEmVideo/Testes/aula08b.py
@@ -0,0 +1,6 @@
+from math import sqrt,floor
+
+num = int(input('digite um numero: '))
+raiz = sqrt(num)
+
+print(f'A raiz de {num} é igual a {floor(raiz)}!')
