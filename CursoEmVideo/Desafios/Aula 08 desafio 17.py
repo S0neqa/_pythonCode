8206da576d96e1988485b5dbb90a850c252064d0
S0neqa <166238806+S0neqa@users.noreply.github.com>
2024-05-25 23:51:17 -0300

    resolução

--- CursoEmVideo/Desafios/Aula 08 desafio 17.py
+++ CursoEmVideo/Desafios/Aula 08 desafio 17.py
@@ -1,9 +1,10 @@
-from math import sqrt
+from math import sqrt, hypot
 
 CO = float(input('qual o valor do cateto oposto? '))
 CA = float(input('qual o valor do cateto adjacente? '))
 
 # H = ((CO**2)+(CA**2))**(1/2)
-H = sqrt(CO*CO+CA*CA)
+# H = sqrt(CO*CO+CA*CA)
+H = hypot(CO, CA)
 
 print(f'a impotenusa é {H}')
